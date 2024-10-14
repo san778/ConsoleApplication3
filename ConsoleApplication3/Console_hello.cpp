081476cb3c24c4d13dcc4a536ab1b742a5512b4b
san778 <4b3g0067@stust.edu.tw>
2024-10-14 20:03:33 +0800

    加入專案檔案。

---
+++ ConsoleApplication3/Console_hello.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+	double x, y, result;
+	char oper;
+
+	Calculator c;
+	while (true) {
+		char ch;
+		cout << "請輸入運算式: ";
+		cin >> x >> oper >> y;
+		result = c.Calculate(x, oper, y);
+		cout << "Result: " << result << endl;
+		cout << "是否繼續? (y/n) ";
+		cin >> ch;
+		if (ch == 'y') {
+			cout << "Continue..." << endl;
+			cout << "Go..." << endl;
+		}
+		else break;
+	}
+}
